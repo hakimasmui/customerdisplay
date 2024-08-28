13bd57122a1e8aea4048b3b1f5f6941e75ec91c1
Hakim <hakimasmui@gmail.com>
2024-08-28 14:13:49 +0700

    gradle

--- customerdisplay/build.gradle.kts
+++ customerdisplay/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.library")
+    id("maven-publish")
 }
 
 android {
@@ -26,8 +27,27 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
 
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.hakimasmui"
+            artifactId = "customer-display"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
